0506beb8dce55af00b98ccbb937efd6fd9a6acca
Sujata Gunale <tidbitsjs@gmail.com>
2020-10-21 19:08:21 +0530

    store search & btns

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./container/Header/Header";
 import About from "./container/About/About";
+import Store from "./container/Store/Store";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <About />
+      <Store />
     </div>
   );
 };
---
+++ src/container/Store/Store.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Container, Row, Col, InputGroup, FormControl } from "react-bootstrap";
+
+class Store extends Component {
+  render() {
+    const sortButtons = ["all", "cake", "cupcake", "sweet", "doughnut"].map(
+      (btn, index) => (
+        <a
+          href={"#" + btn}
+          key={index + btn}
+          className="btn btn-black text-uppercase filter-btn m-2"
+          data-filter={btn}
+        >
+          {btn}
+        </a>
+      )
+    );
+
+    return (
+      <section id="store" className="store py-5">
+        <Container>
+          <Row>
+            <Col xs={10} sm={6} className="mx-auto text-center">
+              <h1 className="text-capitalize">
+                Our <strong className="banner-title">Store</strong>
+              </h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col
+              lg={8}
+              className="mx-auto d-flex justify-content-around sortBtn flex-wrap"
+            >
+              {sortButtons}
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={10} md={6} className="mx-auto">
+              <InputGroup className="mt-3 mb-3">
+                <InputGroup.Prepend id="basic-addon">
+                  <span
+                    className="input-group-text search-box"
+                    id="search-icon"
+                  >
+                    <i className="fas fa-search"></i>
+                  </span>
+                </InputGroup.Prepend>
+                <FormControl
+                  placeholder="Item"
+                  aria-label="Item"
+                  aria-describedby="basic-addon"
+                  id="search-item"
+                />
+              </InputGroup>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    );
+  }
+}
+
+export default Store;
